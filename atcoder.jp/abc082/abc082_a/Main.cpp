5aa5218ad8e055f04345c069f6cd02df0fe89803
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2019-10-22 16:42:24 +0900

    [AC] abc082 abc082_a

---
+++ atcoder.jp/abc082/abc082_a/Main.cpp
@@ -0,0 +1,11 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+
+int main(){
+  int a, b;
+  cin >> a >> b;
+  cout << (a+b+1) /2 << endl;
+}
