31ab69e17a36dd1efa9ce1239ce1b2503777502b
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2019-10-22 16:39:31 +0900

    [AC] abc088 abc088_a

---
+++ atcoder.jp/abc088/abc088_a/Main.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+
+int main(){
+  int N, A;
+  cin >> N >> A;
+  if(A >= N % 500){
+    cout << "Yes" << endl;
+  }
+  else{
+    cout << "No" << endl;
+  }
+}
