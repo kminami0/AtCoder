941035eb77610325feb6e09e9faccbf2b8cebc12
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2019-10-22 16:36:18 +0900

    [AC] abc064 abc064_a

---
+++ atcoder.jp/abc064/abc064_a/Main.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+
+int main(){
+  int r, g, b;
+  cin >> r >> g >> b;
+  if((10 * g + b) % 4 == 0){
+    cout << "YES" << endl;
+  }
+  else{
+    cout << "NO" << endl;
+  }
+}
