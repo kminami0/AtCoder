63418a240adc4ed7d5824bf8845fca05a5a1202d
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2019-11-02 13:20:56 +0900

    [AC] abc053 abc053_b

---
+++ atcoder.jp/abc053/abc053_b/Main.cpp
@@ -0,0 +1,17 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define chmin(x, y) x = min(x, y)
+#define chmax(x, y) x = max(x, y)
+
+int main(){
+  string s;
+  cin >> s;
+  int a = s.find("A");
+  reverse(all(s));
+  int z = s.find("Z");
+  cout << s.size() - z - a << endl;
+}
