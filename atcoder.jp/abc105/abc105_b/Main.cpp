862dd277eed752977ec7ced7a3dccffb5d297121
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2019-11-02 13:35:20 +0900

    [AC] abc105 abc105_b

---
+++ atcoder.jp/abc105/abc105_b/Main.cpp
@@ -0,0 +1,21 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define chmin(x, y) x = min(x, y)
+#define chmax(x, y) x = max(x, y)
+
+int main(){
+  int N;
+  cin >> N;
+  while(N >= 0){
+    if(N % 4 == 0){
+      cout << "Yes" << endl;
+      return 0;
+    }
+    N -= 7;
+  }
+  cout << "No" << endl;
+}
