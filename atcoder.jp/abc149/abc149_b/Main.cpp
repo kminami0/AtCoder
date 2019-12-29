f0be2947c44b58dbc00686c46b7441d4b2f5d44d
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2019-12-29 20:06:49 +0900

    [AC] abc149 abc149_b

---
+++ atcoder.jp/abc149/abc149_b/Main.cpp
@@ -0,0 +1,41 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  int64_t A, B, K;
+  cin >> A >> B >> K;
+  if(K <= A){
+    cout << A - K << " " << B << endl;
+  }
+  else if(K <= A + B){
+    cout << 0 << " " << B - (K - A) << endl;
+  }
+  else{
+    cout << 0 << " " << 0 << endl;
+  }
+}
