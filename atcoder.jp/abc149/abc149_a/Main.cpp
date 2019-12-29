d5be35cc99256d777a9179000556d32a8ebb1636
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2019-12-29 20:02:08 +0900

    [AC] abc149 abc149_a

---
+++ atcoder.jp/abc149/abc149_a/Main.cpp
@@ -0,0 +1,34 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  string S, T;
+  cin >> S >> T;
+  T += S;
+  cout << T << endl;
+}
