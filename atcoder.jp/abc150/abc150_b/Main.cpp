7564057943c6b66d229c90efd1c2f69dde6ef4cb
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-01-10 21:08:19 +0900

    [AC] abc150 abc150_b

---
+++ atcoder.jp/abc150/abc150_b/Main.cpp
@@ -0,0 +1,41 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000000
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  int N;
+  string S;
+  cin >> N >> S;
+  int cnt = 0;
+  rep(i, N-2){
+    if(S.substr(i, 3) == "ABC"){
+      cnt++;
+    }
+  }
+  cout << cnt << endl;
+}
