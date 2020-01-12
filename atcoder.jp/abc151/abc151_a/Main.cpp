37f672b23f0a8f81d77fcbabb0020b9fe62725cb
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-01-12 21:01:06 +0900

    [AC] abc151 abc151_a

---
+++ atcoder.jp/abc151/abc151_a/Main.cpp
@@ -0,0 +1,35 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000000
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  char C;
+  cin >> C;
+  C++;
+  cout << C << endl;
+}
