b3a84bf35eb9c64031ad4fa203a901051139cb12
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-01-19 20:55:54 +0900

    [AC] abc152 abc152_b

---
+++ atcoder.jp/abc152/abc152_b/Main.cpp
@@ -0,0 +1,49 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000000
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  int a, b;
+  cin >> a >> b;
+  ll ans = 0;
+  if(a < b){
+    rep(i, b){
+      ans *= 10;
+      ans += a;
+    }
+    cout << ans << endl;
+  }
+  else{
+    rep(i, a){
+      ans *= 10;
+      ans += b;
+    }
+    cout << ans << endl;
+  }
+}
