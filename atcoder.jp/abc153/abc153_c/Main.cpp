b218595f50b15ff46643c4c415ed94ca259a70fc
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-01-26 21:13:12 +0900

    [AC] abc153 abc153_c

---
+++ atcoder.jp/abc153/abc153_c/Main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000000
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  ll N, K;
+  cin >> N >> K;
+  vector<ll> H(N);
+  rep(i, N){
+    cin >> H.at(i);
+  }
+  sort(all(H));
+  rep(i, min(K, N)){
+    H.pop_back();
+  }
+  ll cnt = 0;
+  rep(i, N-K){
+    cnt += H.at(i);
+  }
+  cout << cnt << endl;
+}
