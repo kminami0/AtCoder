285161f0593a7be07de84f94d6a493ab56904270
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-02-09 21:10:14 +0900

    [AC] abc154 abc154_c

---
+++ atcoder.jp/abc154/abc154_c/Main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000009
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  int N;
+  cin >> N;
+  vector<ll> A(N);
+  rep(i, N){
+    cin >> A.at(i);
+  }
+  set<ll> S;
+  rep(i, N){
+    S.insert(A.at(i));
+  }
+  if(S.size() == N){
+    cout << "YES" << endl;
+  }
+  else{
+    cout << "NO" << endl;
+  }
+}
