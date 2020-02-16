73091a657b69798a851cd731c8de2cb21bafd975
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-02-16 21:05:48 +0900

    [AC] abc155 abc155_b

---
+++ atcoder.jp/abc155/abc155_b/Main.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000009
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  rep(i, N){
+    cin >> A.at(i);
+  }
+  bool j = false;
+  rep(i, N){
+    if(A.at(i) % 2 == 0){
+      if(A.at(i) % 3 != 0 && A.at(i) % 5 != 0){
+        break;
+      }
+    }
+    if(i == N-1){
+      j = true;
+    }
+  }
+  if(j){
+    cout << "APPROVED" << endl;
+  }
+  else{
+    cout << "DENIED" << endl;
+  }
+}
