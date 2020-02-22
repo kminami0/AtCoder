910ec75152b7822d3815ea069655e14248c2fd97
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-02-22 21:05:15 +0900

    [AC] abc156 abc156_b

---
+++ atcoder.jp/abc156/abc156_b/Main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000009
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  ll N, K;
+  cin >> N >> K;
+  ll cnt = 0;
+  while(N != 0){
+    cnt++;
+    N /= K;
+  }
+  cout << cnt << endl;
+}
