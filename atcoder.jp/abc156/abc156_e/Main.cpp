6a5d2f9e738bc009354ddd3fa1f880c8612a3f73
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-02-23 12:18:17 +0900

    [AC] abc156 abc156_e

---
+++ atcoder.jp/abc156/abc156_e/Main.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000009
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const {
+    return pow(mod-2);
+  }
+  mint& operator/=(const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res/=a;
+  }
+};
+
+mint pow2(ll n){
+  mint ans = 1;
+  if(n == 0){
+    return ans;
+  }
+  else if(n % 2 == 0){
+    ans = pow2(n/2);
+    ans *= ans;
+    return ans;
+  }
+  else{
+    ans = pow2(n/2);
+    ans *= ans;
+    ans *= 2;
+    return ans;
+  }
+}
+
+vector<mint> fac(2000003, 1);
+ 
+void calc(int n){
+  rep(i, n){
+    fac.at(i+1) = fac.at(i) * (i+1);
+  }
+  return;
+}
+ 
+mint f(int n, int k){ // nCk
+  mint a = fac.at(n);
+  mint b = fac.at(k);
+  mint d = fac.at(n-k);
+  mint ans = a / (b * d);
+  return ans;
+}
+
+int main(){
+  ll n, k;
+  cin >> n >> k;
+  calc(n);
+  mint ans = 0;
+  rep(i, min(k+1, n)){
+    ans += f(n, i) * f(n - 1, i);
+  }
+  cout << ans.x << endl;
+}
