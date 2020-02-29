f66c4b015f7f1faa8e4f7fa7bee68633b4003d3c
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-02-29 15:11:41 +0900

    [AC] dp dp_i

---
+++ atcoder.jp/dp/dp_i/Main.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000009
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res+=a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res-=a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res*=a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+ 
+  // for prime mod
+  mint inv() const {
+    return pow(mod-2);
+  }
+  mint& operator/=(const mint a) {
+    return (*this) *= a.inv();
+  }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res/=a;
+  }
+};
+
+vector<vector<double>> dp(3000, vector<double>(3000, 0));
+//dp.at(i).at(j)はi枚投げたとき表がj回出ている確率
+
+int main(){
+  ll N;
+  cin >> N;
+  vector<double> p(N);
+  rep(i, N){
+    cin >> p.at(i);
+  }
+  dp.at(0).at(0) = 1;
+  rep(i, N){
+    rep(j, i+2){
+      if(j == 0){
+        dp.at(i+1).at(0) = (1 - p.at(i)) * dp.at(i).at(0);
+      }
+      else{
+        dp.at(i+1).at(j) = p.at(i) * dp.at(i).at(j-1) + (1 - p.at(i)) * dp.at(i).at(j);
+      }
+    }
+  }
+  double ans = 0;
+  rep2(i, N/2 + 1, N+1){
+    ans += dp.at(N).at(i);
+  }
+  cout << fixed << setprecision(10);
+  cout << ans << endl;
+}
