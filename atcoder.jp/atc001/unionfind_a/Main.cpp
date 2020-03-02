663279bcc78646acea24ec78b0706eb6068247ec
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-03-02 22:32:02 +0900

    [AC] atc001 unionfind_a

---
+++ atcoder.jp/atc001/unionfind_a/Main.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000009
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+vector<ll> par(100000);
+vector<ll> rnk(100000);
+
+void init(ll n){
+  rep(i, n){
+    par.at(i) = i;
+    rnk.at(i) = 0;
+  }
+}
+
+ll root(ll x){
+  if(par.at(x) == x){
+    return x;
+  }
+  else{
+    par.at(x) = root(par.at(x));
+    return par.at(x);
+  }
+}
+
+bool same(ll x, ll y){
+  if(root(x) == root(y)){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+void unite(ll x, ll y){
+  x = root(x);
+  y = root(y);
+  if(x == y){
+    return;
+  }
+  if(rnk.at(x) < rnk.at(y)){
+    par.at(x) = y;
+  }
+  else{
+    par.at(y) = x;
+    if(rnk.at(x) == rnk.at(y)){
+      rnk.at(x)++;
+    }
+  }
+}
+
+int main(){
+  ll N, Q;
+  cin >> N >> Q;
+  init(N);
+  vector<ll> P(Q), A(Q), B(Q);
+  rep(i, Q){
+    cin >> P.at(i) >> A.at(i) >> B.at(i);
+  }
+  rep(i, Q){
+    if(P.at(i) == 0){
+      unite(A.at(i), B.at(i));
+    }
+    else{
+      if(same(A.at(i), B.at(i))){
+        cout << "Yes" << endl;
+      }
+      else{
+        cout << "No" << endl;
+      }
+    }
+  }
+}
