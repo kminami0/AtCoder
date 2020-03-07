60680a1e1aaaf019d5cddc4c196c080295c8cfc7
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-03-07 21:09:17 +0900

    [AC] abc158 abc158_c

---
+++ atcoder.jp/abc158/abc158_c/Main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000009
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  ll A, B;
+  cin >> A >> B;
+  rep(i, 1010){
+    if(i*8/100 == A && i*10/100 == B){
+      cout << i << endl;
+      return 0;
+    }
+  }
+  cout << -1 << endl;
+}
