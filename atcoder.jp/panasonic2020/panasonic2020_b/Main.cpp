ab411c6c46c267f724137c3324666cad48b68af9
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-03-14 21:08:41 +0900

    [AC] panasonic2020 panasonic2020_b

---
+++ atcoder.jp/panasonic2020/panasonic2020_b/Main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000009
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  ll H, W;
+  cin >> H >> W;
+  if(H == 1 || W == 1){
+    cout << 1 << endl;
+    return 0;
+  }
+  cout << (W / 2 * H) + (W % 2) * (H + 1) / 2 << endl;
+}
