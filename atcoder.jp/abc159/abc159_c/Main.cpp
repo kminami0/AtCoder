2ef7ff6efb3487721670a4f19ead0df5ebbee911
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-03-22 21:12:32 +0900

    [AC] abc159 abc159_c

---
+++ atcoder.jp/abc159/abc159_c/Main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000009
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  double L;
+  cin >> L;
+  cout << fixed << setprecision(12);
+  cout << (L/3) * (L/3) * (L/3) << endl;
+}
