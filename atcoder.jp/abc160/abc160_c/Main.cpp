ff84ebc59a4105c17e4c7dd628c371f22fc9d60e
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-03-28 21:10:01 +0900

    [AC] abc160 abc160_c

---
+++ atcoder.jp/abc160/abc160_c/Main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1000000009
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  ll K, N;
+  cin >> K >> N;
+  vector<ll> A(N);
+  rep(i, N){
+    cin >> A.at(i);
+  }
+  ll d = 0;
+  rep(i, N-1){
+    chmax(d, A.at(i+1)-A.at(i));
+  }
+  chmax(d, K-A.at(N-1)+A.at(0));
+  cout << K-d << endl;
+}
