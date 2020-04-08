4aadc699c54695affd32f544dc6dfcd5be123ad0
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-04-08 13:23:50 +0900

    [AC] arc054 arc054_b

---
+++ atcoder.jp/arc054/arc054_b/Main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  double P;
+  cin >> P;
+  cout << fixed << setprecision(10);
+  if(1.5*log(log(2)/1.5*P)/log(2) > 0){
+    cout << 1.5*(log(log(2)/1.5*P)+1)/log(2) << endl;
+  }
+  else{
+    cout << P << endl;
+  }
+}
