0c2f9658c9b566af08d9ac8ef8d99f683b5b413e
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-04-12 21:04:58 +0900

    [AC] abc162 abc162_b

---
+++ atcoder.jp/abc162/abc162_b/Main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  ll N;
+  cin >> N;
+  ll ans = 0;
+  rep2(i, 1, N+1){
+    if(i % 3 != 0 && i % 5 != 0){
+      ans += i;
+    }
+  }
+  cout << ans << endl;
+}
