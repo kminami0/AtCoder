55cb9ddaa135a10e201212070906d78328c0c50e
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-04-12 21:15:10 +0900

    [AC] abc162 abc162_c

---
+++ atcoder.jp/abc162/abc162_c/Main.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+unsigned euclidean_gcd(unsigned a, unsigned b) {
+  if(a < b) return euclidean_gcd(b, a);
+  unsigned r;
+  while ((r=a%b)) {
+    a = b;
+    b = r;
+  }
+  return b;
+}
+
+int main(){
+  ll K;
+  cin >> K;
+  ll ans = 0;
+  rep2(i, 1, K+1){
+    rep2(j, 1, K+1){
+      rep2(k, 1, K+1){
+        ans += euclidean_gcd(i, euclidean_gcd(j, k));
+      }
+    }
+  }
+  cout << ans << endl;
+}
