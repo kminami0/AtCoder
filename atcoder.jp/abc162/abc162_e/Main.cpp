4b00100c257528d5e91a40c091f1b55ac405ea0e
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-04-26 15:43:02 +0900

    [AC] abc162 abc162_e

---
+++ atcoder.jp/abc162/abc162_e/Main.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+mint pow2(ll a, ll b){
+  if(b == 0){
+    return 1;
+  }
+  if(b % 2 == 0){
+    mint x = pow2(a, b/2);
+    return x * x;
+  }
+  else{
+    mint x = pow2(a, b/2);
+    return x * x * a;
+  }
+}
+
+int main(){
+  ll N, K;
+  cin >> N >> K;
+  vector<mint> d(K+1, 0), e(K+1, 0);
+  rep2(i, 1, K+1){
+    e.at(i) = pow2(K/i, N);
+  }
+  rep(i, K){
+    d.at(K-i) = e.at(K-i);
+    ll j = 2;
+    while(j * (K - i) <= K){
+      d.at(K-i) -= d.at(j*(K-i));
+      j++;
+    }
+  }
+  mint ans = 0;
+  rep(i, K){
+    ans += d.at(i+1) * (i+1);
+  }
+  cout << ans << endl;
+}
