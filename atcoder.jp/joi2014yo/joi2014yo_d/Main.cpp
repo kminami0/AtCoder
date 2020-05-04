f27df466c6f0eb0041ae61021e5d10876392b356
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-05-04 11:14:12 +0900

    [AC] joi2014yo joi2014yo_d

---
+++ atcoder.jp/joi2014yo/joi2014yo_d/Main.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 10007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+vector<vector<mint>> dp(1001, vector<mint>(8, 0));
+//dp.at(i).at(j)はi日目の参加者がjであるときのスケジュール数
+//Jは0, Oは1, Iは2
+
+int main(){
+  ll N;
+  string S;
+  cin >> N >> S;
+  dp.at(0).at(4) = 1;
+  rep(i, N){
+    rep(j, 8){
+      if(S.at(i) == 'J' && (j & 4) == 0){
+        continue;
+      }
+      if(S.at(i) == 'O' && (j & 2) == 0){
+        continue;
+      }
+      if(S.at(i) == 'I' && (j & 1) == 0){
+        continue;
+      }
+      rep(k, 8){
+        if((j & k) != 0){
+          dp.at(i+1).at(j) += dp.at(i).at(k);
+        }
+      }
+    }
+  }
+  mint ans = 0;
+  rep(j, 8){
+    ans += dp.at(N).at(j);
+  }
+  cout << ans << endl;
+}
