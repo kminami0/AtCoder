e9f198066acb91cbad2c5fd5c6baf28da76f4247
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-05-14 18:26:03 +0900

    [AC] abc084 abc084_d

---
+++ atcoder.jp/abc084/abc084_d/Main.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+// Sieve of Eratosthenes
+// https://youtu.be/UTVg7wzMWQc?t=2774
+struct Sieve {
+  int n;
+  vector<int> f, primes;
+  Sieve(int n=1):n(n), f(n+1) {
+    f[0] = f[1] = -1;
+    for (ll i = 2; i <= n; ++i) {
+      if (f[i]) continue;
+      primes.push_back(i);
+      f[i] = i;
+      for (ll j = i*i; j <= n; j += i) {
+        if (!f[j]) f[j] = i;
+      }
+    }
+  }
+  bool isPrime(int x) { return f[x] == x;}
+  vector<int> factorList(int x) {
+    vector<int> res;
+    while (x != 1) {
+      res.push_back(f[x]);
+      x /= f[x];
+    }
+    return res;
+  }
+  vector<P> factor(int x) {
+    vector<int> fl = factorList(x);
+    if (fl.size() == 0) return {};
+    vector<P> res(1, P(fl[0], 0));
+    for (int p : fl) {
+      if (res.back().first == p) {
+        res.back().second++;
+      } else {
+        res.emplace_back(p, 1);
+      }
+    }
+    return res;
+  }
+};
+
+int main(){
+  ll Q;
+  cin >> Q;
+  vector<ll> l(Q), r(Q);
+  rep(i, Q){
+    cin >> l.at(i) >> r.at(i);
+  }
+  Sieve S = {100001};
+  vector<ll> like(100001, 0);
+  rep2(i, 3, 100001){
+    if(S.isPrime(i)){
+      if(S.isPrime((i+1)/2)){
+        like.at(i) = like.at(i-1)+1;
+      }
+      else{
+        like.at(i) = like.at(i-1);
+      }
+    }
+    else{
+      like.at(i) = like.at(i-1);
+    }
+  }
+  rep(i, Q){
+    cout << like.at(r.at(i))-like.at(l.at(i)-1) << endl;
+  }
+}
