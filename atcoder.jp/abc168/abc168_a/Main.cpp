738bfe90503f3432644bbebbdad5642e72e9a262
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-05-17 21:02:30 +0900

    [AC] abc168 abc168_a

---
+++ atcoder.jp/abc168/abc168_a/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  ll N;
+  cin >> N;
+  if(N % 10 == 2 || N % 10 == 4 || N % 10 == 5 || N % 10 == 7 || N % 10 == 9){
+    cout << "hon" << endl;
+  }
+  else if(N % 10 == 3){
+    cout << "bon" << endl;
+  }
+  else{
+    cout << "pon" << endl;
+  }
+}
