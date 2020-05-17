dc4783623dab2f59a907ee6db8e598350423b12e
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-05-17 22:24:33 +0900

    [AC] abc168 abc168_c

---
+++ atcoder.jp/abc168/abc168_c/Main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  double A, B, H, M;
+  cin >> A >> B >> H >> M;
+  double t = 60*H+M;
+  double x = t/2;
+  double y = M * 6;
+  cout << fixed << setprecision(20);
+  cout << sqrt(B*B + A*A - 2.0 * A * B * cos((y-x)*M_PI/180)) << endl;
+}
