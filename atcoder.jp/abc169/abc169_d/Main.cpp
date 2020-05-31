988d63ff04ad30ed28d104fe1909ef517c3b62c9
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-05-31 21:41:49 +0900

    [AC] abc169 abc169_d

---
+++ atcoder.jp/abc169/abc169_d/Main.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<ll, ll>;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+vector<pair<long long, long long> > prime_factorize(long long N) {
+    vector<pair<long long, long long> > res;
+    for (long long a = 2; a * a <= N; ++a) {
+        if (N % a != 0) continue;
+        long long ex = 0; // 指数
+
+        // 割れる限り割り続ける
+        while (N % a == 0) {
+            ++ex;
+            N /= a;
+        }
+
+        // その結果を push
+        res.push_back({a, ex});
+    }
+
+    // 最後に残った数について
+    if (N != 1) res.push_back({N, 1});
+    return res;
+}
+
+int main(){
+  ll N;
+  cin >> N;
+  const auto &res = prime_factorize(N);
+  ll ans = 0;
+  for(auto p : res){
+    ll e = 1;
+    while(true){
+      if(e * (e+1) / 2 <= p.second){
+        e++;
+      }
+      else{
+        e--;
+        break;
+      }
+    }
+    ans += e;
+  }
+  cout << ans << endl;
+}
