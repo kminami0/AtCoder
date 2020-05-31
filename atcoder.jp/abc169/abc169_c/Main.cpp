937f5f10e7f2110093dc7e5f84c0c7b44d620f98
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-05-31 21:57:15 +0900

    [AC] abc169 abc169_c

---
+++ atcoder.jp/abc169/abc169_c/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  ll A;
+  string B;
+  cin >> A >> B;
+  ll B1, B2, B3;
+  B1 = (B.at(0) - '0') * 100;
+  B2 = (B.at(2) - '0') * 10;
+  B3 = B.at(3) - '0';
+  ll C = B1 + B2 + B3;
+  ll D = A * C;
+  ll E = D / 100;
+  cout << E << endl;
+}
