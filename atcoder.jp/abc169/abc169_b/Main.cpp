bb882b11f62944a6814b9724103e2009ff9f8707
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-05-31 21:07:38 +0900

    [AC] abc169 abc169_b

---
+++ atcoder.jp/abc169/abc169_b/Main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  ll N;
+  cin >> N;
+  vector<ll> A(N);
+  rep(i, N){
+    cin >> A.at(i);
+    if(A.at(i) == 0){
+      cout << 0 << endl;
+      return 0;
+    }
+  }
+  ll ans = 1;
+  rep(i, N){
+    if(ans > 1000000000000000000 / A.at(i)){
+      cout << -1 << endl;
+      return 0;
+    }
+    ans *= A.at(i);
+  }
+  cout << ans << endl;
+}
