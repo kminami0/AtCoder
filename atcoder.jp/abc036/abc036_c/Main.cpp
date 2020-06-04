9fa7ca5f5c96e4dd36ba7722daf3dd467f407acb
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-06-04 21:39:20 +0900

    [AC] abc036 abc036_c

---
+++ atcoder.jp/abc036/abc036_c/Main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14 100兆
+// aよりもbが大きいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+// aよりもbが小さいならばaをbで更新する
+// (更新されたならばtrueを返す)
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;  // aをbで更新
+    return true;
+  }
+  return false;
+}
+
+int main(){
+  ll N;
+  cin >> N;
+  vector<ll> a(N);
+  vector<ll> b(N);
+  rep(i, N){
+    cin >> a.at(i);
+    b.at(i) = a.at(i);
+  }
+  sort(all(b));
+  b.erase(unique(all(b)), b.end());
+  map<ll, ll> pos;
+  rep(i, sz(b)){
+    pos[b.at(i)] = i;
+  }
+  rep(i, N){
+    cout << pos.at(a.at(i)) << endl;
+  }
+}
