43bd7ff93472e3245b37f82071a3fc0795a58215
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-06-21 21:03:00 +0900

    [AC] abc171 abc171_b

---
+++ atcoder.jp/abc171/abc171_b/Main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N, K;
+    cin >> N >> K;
+    vector<ll> p(N);
+    rep(i, N) cin >> p.at(i);
+    sort(all(p));
+    ll ans = 0;
+    rep(i, K){
+        ans += p.at(i);
+    }
+    cout << ans << endl;
+}
