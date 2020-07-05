26dbfd8bc7ac76d8e9cfad1663ded52eba380954
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-07-05 21:31:35 +0900

    [AC] abc173 abc173_d

---
+++ atcoder.jp/abc173/abc173_d/Main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+  ll N;
+  cin >> N;
+  vector<ll> A(N);
+  rep(i, N) cin >> A.at(i);
+  sort(all(A));
+  reverse(all(A));
+  ll ans = 0;
+  rep(i, N-1){
+    if(i == 0){
+      ans += A.at(0);
+    }
+    else{
+      ans += A.at((i+1)/2);
+    }
+  }
+  cout << ans << endl;
+}
