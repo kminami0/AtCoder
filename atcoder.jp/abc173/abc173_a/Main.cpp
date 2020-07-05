862be4d2d9d2c2d5dec322bfe3e8100239d19505
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-07-05 21:02:08 +0900

    [AC] abc173 abc173_a

---
+++ atcoder.jp/abc173/abc173_a/Main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+  ll N;
+  cin >> N;
+  if(N % 1000 == 0){
+    cout << 0 << endl;
+  }
+  else{
+    cout << 1000 - N % 1000 << endl;
+  }
+}
