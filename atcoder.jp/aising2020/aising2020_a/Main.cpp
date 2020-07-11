079d822911098f1c1d62480d40dfc68528f6bfa7
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-07-11 21:01:34 +0900

    [AC] aising2020 aising2020_a

---
+++ atcoder.jp/aising2020/aising2020_a/Main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll L, R, d;
+    cin >> L >> R >> d;
+    ll ans = 0;
+    rep(i, R-L+1){
+        if((L+i)%d == 0){
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
