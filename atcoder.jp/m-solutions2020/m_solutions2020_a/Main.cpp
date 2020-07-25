13da81c7d2e14d34c305007526811f0598d0e90e
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-07-25 21:02:19 +0900

    [AC] m-solutions2020 m_solutions2020_a

---
+++ atcoder.jp/m-solutions2020/m_solutions2020_a/Main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll X;
+    cin >> X;
+    ll a = (X-400) / 200;
+    cout << 8-a << endl;
+}
