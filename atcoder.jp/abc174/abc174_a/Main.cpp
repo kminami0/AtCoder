948b978f6b9afa634ec5d866b5e505adced2a5ad
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-08-02 21:00:51 +0900

    [AC] abc174 abc174_a

---
+++ atcoder.jp/abc174/abc174_a/Main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll X;
+    cin >> X;
+    if(X >= 30){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl;
+    }
+}
