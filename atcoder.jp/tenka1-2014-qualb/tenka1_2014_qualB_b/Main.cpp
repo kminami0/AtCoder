29ab597c83ac5229995e5122bbf7920a26940d12
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-08-12 16:51:26 +0900

    [AC] tenka1-2014-qualb tenka1_2014_qualB_b

---
+++ atcoder.jp/tenka1-2014-qualb/tenka1_2014_qualB_b/Main.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+  if ((x += a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator-=(const mint a) {
+  if ((x += mod-a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+  if (!t) return 1;
+  mint a = pow(t>>1);
+  a *= a;
+  if (t&1) a *= *this;
+  return a;
+  }
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+
+vector<mint> dp(1001, 0);
+
+int main()
+{
+    ll N;
+    string S;
+    cin >> N >> S;
+    vector<string> T(N);
+    rep(i, N){
+        cin >> T.at(i);
+    }
+    dp.at(0) = 1;
+    rep(i, sz(S)){
+        rep(j, N){
+            if(i+1 < sz(T.at(j))){
+                continue;
+            }
+            if(S.substr(i+1-sz(T.at(j)), sz(T.at(j))) == T.at(j)){
+                dp.at(i+1) += dp.at(i+1-sz(T.at(j)));
+            }
+        }
+    }
+    cout << dp.at(sz(S)) << endl;
+}
