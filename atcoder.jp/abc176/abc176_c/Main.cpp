d11a1373995f6e2575d96f47fa0251a02f7b937b
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-08-22 21:06:28 +0900

    [AC] abc176 abc176_c

---
+++ atcoder.jp/abc176/abc176_c/Main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    rep(i, N) cin >> A.at(i);
+    ll ans = 0;
+    rep(i, N-1){
+        if(A.at(i+1) < A.at(i)){
+            ans += A.at(i) - A.at(i+1);
+            A.at(i+1) = A.at(i);
+        }
+    }
+    cout << ans << endl;
+}
