6d570e6a647b6e7d82f6d3954ab50ee3bb064c33
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-08-29 21:19:03 +0900

    [AC] abc177 abc177_c

---
+++ atcoder.jp/abc177/abc177_c/Main.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+  if ((x += a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator-=(const mint a) {
+  if ((x += mod-a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+  if (!t) return 1;
+  mint a = pow(t>>1);
+  a *= a;
+  if (t&1) a *= *this;
+  return a;
+  }
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    rep(i, N) cin >> A.at(i);
+    vector<mint> acc(N+1);
+    acc.at(0) = 0;
+    rep(i, N){
+        acc.at(i+1) = acc.at(i) + A.at(i);
+    }
+    mint ans = 0;
+    rep(i, N-1){
+        ans += (acc.at(N) - acc.at(i+1)) * A.at(i);
+    }
+    cout << ans << endl;
+}
