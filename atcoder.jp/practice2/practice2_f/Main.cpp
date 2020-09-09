59451910e48b8d323054108c8b3538111888d45a
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-09-09 13:08:04 +0900

    [AC] practice2 practice2_f

---
+++ atcoder.jp/practice2/practice2_f/Main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N, M;
+    cin >> N >> M;
+    vector<ll> a(N);
+    rep(i, N) cin >> a.at(i);
+    vector<ll> b(M);
+    rep(i, M) cin >> b.at(i);
+    vector<ll> ans = convolution(a, b);
+    rep(i, N+M-2){
+      cout << ans.at(i) << " ";
+    }
+    cout << ans.at(N+M-2) << endl;
+}
