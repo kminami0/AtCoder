e593d59af156d2f09d845f2a3bea6027775fa300
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-09-19 21:13:22 +0900

    [AC] abc179 abc179_c

---
+++ atcoder.jp/abc179/abc179_c/Main.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+vector<ll> enum_div(ll n){
+    vector<ll> ret;
+    for(ll i = 1 ; i*i <= n ; ++i){
+        if(n%i == 0){
+            ret.push_back(i);
+           if(i*i != n){
+               ret.push_back(n/i);
+           }
+       }
+   }
+   return ret;
+}
+
+// Sieve of Eratosthenes
+// https://youtu.be/UTVg7wzMWQc?t=2774
+struct Sieve {
+  using P = pair<int, int>;
+  int n;
+  vector<int> f, primes; //f.at(x)はxの最小素因数
+  Sieve(int n=1):n(n), f(n+1) {
+  f[0] = f[1] = -1;
+  for (ll i = 2; i <= n; ++i) {
+    if (f[i]) continue; //ここでf[i]==0なら素数
+    primes.push_back(i);
+    f[i] = i;
+    for (ll j = i*i; j <= n; j += i) {
+  if (!f[j]) f[j] = i;
+    }
+  }
+  }
+  bool isPrime(int x) { return f[x] == x;}
+  vector<int> factorList(int x) { //xの素因数を小さい順に列挙
+  vector<int> res;
+  while (x != 1) {
+    res.push_back(f[x]);
+    x /= f[x];
+  }
+  return res;
+  }
+  vector<P> factor(int x) {
+  vector<int> fl = factorList(x);
+  if (fl.size() == 0) return {};
+  vector<P> res(1, P(fl[0], 0));
+  for (int p : fl) { //ここでランレングス圧縮
+    if (res.back().first == p) {
+  res.back().second++;
+    } else {
+  res.emplace_back(p, 1);
+    }
+  }
+  return res;
+  }
+};
+// Sieve sieve(N);でN以下の各自然数の最小素因数を調べ上げる
+// sieve.factor(M)でMの素因数分解
+
+int main()
+{
+    ll N;
+    cin >> N;
+    Sieve sieve(N);
+    ll ans = 0;
+    rep2(i, 1, N){
+        vector<pair<int, int>> P = sieve.factor(N-i);
+        ll c = 1;
+        rep(j, sz(P)){
+            c *= (P.at(j).second+1);
+        }
+        ans += c;
+    }
+    cout << ans << endl;
+}
