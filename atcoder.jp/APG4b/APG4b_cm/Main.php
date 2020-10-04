6a76e3a30611587d8e7b61e6f978490c9b8af49c
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-10-04 13:19:36 +0900

    [AC] APG4b APG4b_cm

---
+++ atcoder.jp/APG4b/APG4b_cm/Main.php
@@ -0,0 +1,15 @@
+<?php
+  fscanf(STDIN, "%d %d", $A, $B);
+  
+  echo "A:";
+  for($i = 0; $i < $A; $i++){
+    echo "]";
+  }
+  echo "\n";
+
+  echo "B:";
+  for($i = 0; $i < $B; $i++){
+    echo "]";
+  }
+  echo "\n";
+?>
