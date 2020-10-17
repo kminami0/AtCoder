55ea08d63c1feda6923a5342963f46207d6ce511
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-10-17 20:07:43 +0900

    [AC] abc180 abc180_c

---
+++ atcoder.jp/abc180/abc180_c/Main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+vector<ll> enum_div(ll n){
+    vector<ll> ret;
+    for(ll i = 1 ; i*i <= n ; ++i){
+        if(n%i == 0){
+            ret.push_back(i);
+           if(i*i != n){
+               ret.push_back(n/i);
+           }
+       }
+   }
+   return ret;
+}
+
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> ans = enum_div(N);
+    sort(all(ans));
+    rep(i, sz(ans)){
+        cout << ans.at(i) << endl;
+    }
+}
