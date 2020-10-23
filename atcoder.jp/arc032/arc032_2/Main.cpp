6ed8a565ebee6305eeabc3aa3f3e4f2ba15c20fd
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-10-23 12:41:57 +0900

    [AC] arc032 arc032_2

---
+++ atcoder.jp/arc032/arc032_2/Main.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+vector<ll> par(1000000);
+vector<ll> rnk(1000000);
+
+void init(ll n){
+  rep(i, n){
+    par.at(i) = i;
+    rnk.at(i) = 0;
+  }
+}
+
+ll root(ll x){
+  if(par.at(x) == x){
+    return x;
+  }
+  else{
+    par.at(x) = root(par.at(x));
+    return par.at(x);
+  }
+}
+
+bool same(ll x, ll y){
+  if(root(x) == root(y)){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+void unite(ll x, ll y){
+  x = root(x);
+  y = root(y);
+  if(x == y){
+    return;
+  }
+  if(rnk.at(x) < rnk.at(y)){
+    par.at(x) = y;
+  }
+  else{
+    par.at(y) = x;
+    if(rnk.at(x) == rnk.at(y)){
+      rnk.at(x)++;
+    }
+  }
+}
+
+//union-findを用いて連結成分の個数を調べ、その数-1が答え
+int main()
+{
+    ll N, M;
+    cin >> N >> M;
+    init(N);
+    rep(i, M){
+        ll a, b;
+        cin >> a >> b;
+        a--;
+        b--;
+        unite(a, b);
+    }
+    set<ll> cc;
+    rep(i, N){
+        cc.insert(root(i));
+    }
+    cout << sz(cc) - 1 << endl;
+}
