0e0386378d7540dcdebfda93f7c0e8a22fb4fb93
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-11-01 21:03:34 +0900

    [AC] abc181 abc181_b

---
+++ atcoder.jp/abc181/abc181_b/Main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> A(N), B(N);
+    rep(i, N){
+        cin >> A.at(i) >> B.at(i);
+    }
+    ll ans = 0;
+    rep(i, N){
+        ans += (A.at(i)+B.at(i)) * (B.at(i)-A.at(i)+1) / 2;
+    }
+    cout << ans << endl;
+}
