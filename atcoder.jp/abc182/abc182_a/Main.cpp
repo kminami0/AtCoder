d2b0b865dcf7722eb2d1402369f333346e4205bb
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-11-08 21:01:12 +0900

    [AC] abc182 abc182_a

---
+++ atcoder.jp/abc182/abc182_a/Main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll A, B;
+    cin >> A >> B;
+    cout << 2*A+100-B << endl;
+}
