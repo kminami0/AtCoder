d0873abdb1d090495b114d9e5f3d665428a2fc9e
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-11-12 12:15:24 +0900

    [AC] hhkb2020 hhkb2020_e

---
+++ atcoder.jp/hhkb2020/hhkb2020_e/Main.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+  if ((x += a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator-=(const mint a) {
+  if ((x += mod-a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+  if (!t) return 1;
+  mint a = pow(t>>1);
+  a *= a;
+  if (t&1) a *= *this;
+  return a;
+  }
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+mint mpow(mint x, ll y){
+  if(y == 0){
+    return 1;
+  }
+  if(y % 2 == 0){
+    mint a = mpow(x, y / 2);
+    return a * a;
+  }
+  else{
+    mint a = mpow(x, y / 2);
+    return a * a * x;
+  }
+}
+
+int main()
+{
+    ll H, W;
+    cin >> H >> W;
+    vector<string> S(H);
+    rep(i, H){
+      cin >> S.at(i);
+    }
+    mint ans = 0;
+    vector<vector<ll>> clut_r(H, vector<ll>(1, -1));
+    rep(i, H){
+      rep(j, W){
+        if(S.at(i).at(j) == '#'){
+          clut_r.at(i).push_back(j);
+        }
+      }
+      clut_r.at(i).push_back(W);
+    }
+    vector<vector<ll>> clut_c(W, vector<ll>(1, -1));
+    rep(i, W){
+      rep(j, H){
+        if(S.at(j).at(i) == '#'){
+          clut_c.at(i).push_back(j);
+        }
+      }
+      clut_c.at(i).push_back(H);
+    }
+    vector<vector<ll>> space(H, vector<ll>(W, 0)); //各マスを照らす照明を置けるマスの数
+    rep(i, H){
+      ll p = 0;
+      rep(j, W){
+        ll left = clut_r.at(i).at(p);
+        ll right = clut_r.at(i).at(p+1);
+        if(S.at(i).at(j) == '#'){
+          p++;
+          continue;
+        }
+        space.at(i).at(j) += right-left-1;
+      }
+    }
+    rep(i, W){
+      ll p = 0;
+      rep(j, H){
+        ll left = clut_c.at(i).at(p);
+        ll right = clut_c.at(i).at(p+1);
+        if(S.at(j).at(i) == '#'){
+          p++;
+          continue;
+        }
+        space.at(j).at(i) += right-left-2;
+      }
+    }
+    ll K = 0;
+    rep(i, H){
+      rep(j, W){
+        if(S.at(i).at(j) == '.'){
+          K++;
+        }
+      }
+    }
+    rep(i, H){
+      rep(j, W){
+        if(S.at(i).at(j) == '.'){
+          ans += mpow(2, K) - mpow(2, K-space.at(i).at(j)); //全体 - (i,j)を照らさない置き方
+        }
+      }
+    }
+    cout << ans << endl;
+}
