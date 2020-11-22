8aa813b9138ebe6f1748cc560fbd9d8f5c124ddc
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-11-22 21:03:04 +0900

    [AC] abc184 abc184_b

---
+++ atcoder.jp/abc184/abc184_b/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N, X;
+    string S;
+    cin >> N >> X >> S;
+    rep(i, sz(S)){
+        if(S.at(i) == 'o'){
+            X++;
+        }
+        else{
+            X--;
+        }
+        if(X < 0){
+            X = 0;
+        }
+    }
+    cout << X << endl;
+}
