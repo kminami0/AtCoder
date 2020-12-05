b193b8620aeb5b7757bbe299e821a7197b9dd935
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-12-05 21:08:48 +0900

    [AC] arc110 arc110_a

---
+++ atcoder.jp/arc110/arc110_a/Main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    ll ans = 1;
+    ans *= 16;
+    ans *= 27;
+    ans *= 25;
+    ans *= 7;
+    ans *= 11;
+    ans *= 13;
+    ans *= 17;
+    ans *= 19;
+    ans *= 23;
+    ans *= 29;
+    cout << ans+1 << endl;
+}
