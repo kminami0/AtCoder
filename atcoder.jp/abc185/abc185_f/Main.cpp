30c00d976f182d1aa978d2960bce08da2680b8cb
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-12-13 21:40:43 +0900

    [AC] abc185 abc185_f

---
+++ atcoder.jp/abc185/abc185_f/Main.cpp
@@ -0,0 +1,76 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+ll op(ll a, ll b) {
+    return a ^ b;
+}
+
+ll e() {
+    return 0;
+}
+
+int main()
+{
+    ll N, Q;
+    cin >> N >> Q;
+    segtree<ll, op, e> seg(N);
+    rep(i, N) {
+        ll A;
+        cin >> A;
+        seg.set(i, A);
+    }
+    rep(i, Q) {
+        ll T, X, Y;
+        cin >> T >> X >> Y;
+        if (T == 1) {
+            X--;
+            ll z = op(seg.get(X), Y);
+            seg.set(X, z);
+        }
+        else {
+            X--;
+            Y--;
+            cout << seg.prod(X, Y + 1) << endl;
+        }
+    }
+}
