c21b4f55b1eed58659cadef7d838d520df36a9d6
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2020-12-13 21:00:59 +0900

    [AC] abc185 abc185_a

---
+++ atcoder.jp/abc185/abc185_a/Main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 100000000000000 //10^14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    vector<ll> A(4);
+    rep(i, 4) cin >> A.at(i);
+    cout << min(A.at(0), min(A.at(1), min(A.at(2), A.at(3)))) << endl;
+}
