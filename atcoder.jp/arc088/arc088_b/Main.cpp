0b5e5f3326c094922909b40cffd1936b748674e9
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-01-05 11:01:09 +0900

    [AC] arc088 arc088_b

---
+++ atcoder.jp/arc088/arc088_b/Main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 10000000000000000 //10^16
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    string S;
+    cin >> S;
+    ll T = sz(S);
+    rep(i, sz(S)-1){
+        if(S.at(i) != S.at(i+1)){
+            chmin(T, max(i+1, ll(sz(S))-i-1));
+        }
+    }
+    cout << T << endl;
+}
