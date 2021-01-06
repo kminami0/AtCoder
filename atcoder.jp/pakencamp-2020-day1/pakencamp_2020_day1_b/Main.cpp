0f0219f1a14ef172e3f1e9a8da352e9549192f18
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-01-06 10:32:03 +0900

    [AC] pakencamp-2020-day1 pakencamp_2020_day1_b

---
+++ atcoder.jp/pakencamp-2020-day1/pakencamp_2020_day1_b/Main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 10000000000000000 //10^16
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    string X;
+    cin >> X;
+    int ans = 0;
+    rep(i, 4){
+      chmax(ans, X.at(i)-'0');
+    }
+    cout << ans << endl;
+}
