56209fffeb27fa8a82f0f3a0d44442c098170d9e
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-01-23 21:16:16 +0900

    [AC] abc189 abc189_b

---
+++ atcoder.jp/abc189/abc189_b/Main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1000000000000000000 //10^17
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N, X;
+    cin >> N >> X;
+    X *= 100;
+    ll sake = 0;
+    rep(i, N){
+      ll V, P;
+      cin >> V >> P;
+      sake += V * P;
+      if(sake > X){
+        cout << i+1 << '\n';
+        return 0;
+      }
+    }
+    cout << -1 << '\n';
+}
