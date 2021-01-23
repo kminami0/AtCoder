6ed967ce8793f03f2bc899f34da3054cca76f905
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-01-23 21:00:55 +0900

    [AC] abc189 abc189_a

---
+++ atcoder.jp/abc189/abc189_a/Main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1000000000000000000 //10^17
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    string C;
+    cin >> C;
+    if(C.at(0) == C.at(1) && C.at(1) == C.at(2)){
+        cout << "Won" << '\n';
+    }
+    else{
+        cout << "Lost" << '\n';
+    }
+}
