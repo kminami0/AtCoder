ebe85d53cf748ddedb04e071546bd4f7488a5dbc
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-01-30 21:03:54 +0900

    [AC] abc190 abc190_b

---
+++ atcoder.jp/abc190/abc190_b/Main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1000000000000000000 //10^17
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+//浮動小数点はなるべく使うな、powも気をつけろ
+int main()
+{
+    ll N, S, D;
+    cin >> N >> S >> D;
+    vector<ll> X(N), Y(N);
+    rep(i, N){
+        cin >> X.at(i) >> Y.at(i);
+        if(X.at(i) < S && Y.at(i) > D){
+            cout << "Yes" << '\n';
+            return 0;
+        }
+    }
+    cout << "No" << '\n';
+}
