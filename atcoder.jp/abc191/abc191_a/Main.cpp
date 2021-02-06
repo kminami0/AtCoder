0c2d93f01eac50b20b3d5b0a6ba949f7b31653a0
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-02-06 21:02:10 +0900

    [AC] abc191 abc191_a

---
+++ atcoder.jp/abc191/abc191_a/Main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1000000000000000000 //10^17
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+//浮動小数点はなるべく使うな、powも気をつけろ
+int main()
+{
+    ll V, T, S, D;
+    cin >> V >> T >> S >> D;
+    if(T*V <= D && D <= S*V){
+        cout << "No" << '\n';
+    }
+    else{
+        cout << "Yes" << '\n';
+    }
+}
