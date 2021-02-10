d011f235e578b2d85caacaa75b150d4307443a32
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-02-10 11:18:13 +0900

    [AC] abc130 abc130_e

---
+++ atcoder.jp/abc130/abc130_e/Main.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1000000000000000000 //10^17
+#define EPSILON 0.00000000000001 // 10^-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+  if ((x += a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator-=(const mint a) {
+  if ((x += mod-a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+  if (!t) return 1;
+  mint a = pow(t>>1);
+  a *= a;
+  if (t&1) a *= *this;
+  return a;
+  }
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+//浮動小数点はなるべく使うな、powも気をつけろ
+int main()
+{
+    ll N, M;
+    cin >> N >> M;
+    vector<ll> S(N);
+    rep(i, N) cin >> S.at(i);
+    vector<ll> T(M);
+    rep(i, M) cin >> T.at(i);
+    vector<vector<mint>> dp(N+1, vector<mint>(M+1));
+    rep(i, M+1){
+        dp.at(0).at(i) = 1;
+    }
+    rep(i, N+1){
+        dp.at(i).at(0) = 1;
+    }
+    rep(i, N){
+        rep(j, M){
+            if(S.at(i) == T.at(j)){
+                dp.at(i+1).at(j+1) = dp.at(i).at(j+1) + dp.at(i+1).at(j);
+            }
+            else{
+                dp.at(i+1).at(j+1) = dp.at(i).at(j+1) + dp.at(i+1).at(j) - dp.at(i).at(j);
+            }
+        }
+    }
+    cout << dp.at(N).at(M) << '\n';
+}
