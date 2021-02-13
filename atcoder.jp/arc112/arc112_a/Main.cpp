67a67b2d5a2939cab8de7ee810bf1f4837cf0239
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-02-13 21:07:41 +0900

    [AC] arc112 arc112_a

---
+++ atcoder.jp/arc112/arc112_a/Main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1000000000000000000 //10^17
+#define EPSILON 0.00000000000001 // 10^-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+//浮動小数点はなるべく使うな、powも気をつけろ
+int main()
+{
+    ll T;
+    cin >> T;
+    rep(i, T){
+        ll L, R;
+        cin >> L >> R;
+        if(R - 2*L + 1 < 1){
+            cout << 0 << '\n';
+        }
+        else{
+            cout << (R - 2*L + 1) * (R - 2*L + 2) / 2 << '\n';
+        }
+    }
+}
