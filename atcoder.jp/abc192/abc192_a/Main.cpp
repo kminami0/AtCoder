0a1a001170b2eefd4891822bae5285bb6894b408
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-02-20 21:01:01 +0900

    [AC] abc192 abc192_a

---
+++ atcoder.jp/abc192/abc192_a/Main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1000000000000000000 //10^17
+#define EPSILON 0.00000000000001 // 10^-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+//浮動小数点はなるべく使うな、powも気をつけろ
+int main()
+{
+    ll X;
+    cin >> X;
+    cout << 100 - (X % 100) << endl;
+}
