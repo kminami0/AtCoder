c1bb3105ed341aeebd6a79fc412734ccc7a6fdc4
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-02-21 21:02:56 +0900

    [AC] arc113 arc113_a

---
+++ atcoder.jp/arc113/arc113_a/Main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1000000000000000000 //10^17
+#define EPSILON 0.00000000000001 // 10^-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+//浮動小数点はなるべく使うな、powも気をつけろ
+int main()
+{
+    ll K;
+    cin >> K;
+    ll ans = 0;
+    rep2(i, 1, K+1){
+        rep2(j, 1, K/i + 1){
+            ans += K/(i*j);
+        }
+    }
+    cout << ans << '\n';
+}
