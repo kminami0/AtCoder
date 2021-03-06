85f448efe9ccef5a922556b6d12aa3e21c7945f6
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-03-06 21:20:04 +0900

    [AC] abc194 abc194_d

---
+++ atcoder.jp/abc194/abc194_d/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e17
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+struct edge {
+  ll to;
+};
+vector<vector<edge>> graph;
+
+int main()
+{
+    long double N;
+    cin >> N;
+    long double ans = 0;
+    rep(i, N-1){
+        ans += N / (N-1-i);
+    }
+    cout << fixed << setprecision(11);
+    cout << ans << '\n';
+}
