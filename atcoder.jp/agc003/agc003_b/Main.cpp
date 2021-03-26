1a742c74526d8c8fa0d77d0d84806e454a15eedd
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-03-26 21:33:29 +0900

    [AC] agc003 agc003_b

---
+++ atcoder.jp/agc003/agc003_b/Main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+9
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+//浮動小数点はなるべく使うな、powも気をつけろ
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    rep(i, N) cin >> A.at(i);
+    ll ans = 0;
+    rep(i, N){
+        ans += A.at(i) / 2;
+        A.at(i) %= 2;
+        if(i != N-1 && A.at(i) == 1 && A.at(i+1) > 0){
+            ans++;
+            A.at(i)--;
+            A.at(i+1)--;
+        }
+    }
+    cout << ans << '\n';
+}
