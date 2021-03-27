38361f6c9f8eb59fc38581a7152de0a9a311fdbb
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-03-27 21:01:15 +0900

    [AC] abc197 abc197_a

---
+++ atcoder.jp/abc197/abc197_a/Main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+9
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+//浮動小数点はなるべく使うな、powも気をつけろ
+int main()
+{
+    string S;
+    cin >> S;
+    string ans = "";
+    ans += S.at(1);
+    ans += S.at(2);
+    ans += S.at(0);
+    cout << ans << '\n';
+}
