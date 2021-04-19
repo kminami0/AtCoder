24c8d21560046e69ec8da51d5904f1e1e6cec7e4
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-04-19 15:04:59 +0900

    [AC] arc044 arc044_b

---
+++ atcoder.jp/arc044/arc044_b/Main.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+9
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 1000000007;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+  if ((x += a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator-=(const mint a) {
+  if ((x += mod-a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+  if (!t) return 1;
+  mint a = pow(t>>1);
+  a *= a;
+  if (t&1) a *= *this;
+  return a;
+  }
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+mint mpow(mint x, ll y){
+  if(y == 0){
+    return 1;
+  }
+  if(y % 2 == 0){
+    mint a = mpow(x, y / 2);
+    return a * a;
+  }
+  else{
+    mint a = mpow(x, y / 2);
+    return a * a * x;
+  }
+}
+
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> nodes(N, 0);
+    rep(i, N){
+        ll A;
+        cin >> A;
+        nodes.at(A)++;
+        if(i == 0 && A != 0){
+            cout << 0 << '\n';
+            return 0;
+        }
+    }
+    if(nodes.at(0) != 1){
+        cout << 0 << '\n';
+        return 0;
+    }
+    mint ans = 1;
+    ll cnt = 1;
+    rep(i, N-1){
+        ans *= mpow(mpow(2, nodes.at(i))-1, nodes.at(i+1));
+        ans *= mpow(2, nodes.at(i+1)*(nodes.at(i+1)-1)/2);
+        cnt += nodes.at(i+1);
+        if(cnt == N){
+            break;
+        }
+    }
+    cout << ans << '\n';
+}
