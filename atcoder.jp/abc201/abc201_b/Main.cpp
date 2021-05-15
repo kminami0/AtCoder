d8b5b83b554f80b11edbf2ae739629c5f9557ef6
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-05-15 21:04:02 +0900

    [AC] abc201 abc201_b

---
+++ atcoder.jp/abc201/abc201_b/Main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+9
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<pair<ll, string>> mount(N);
+    rep(i, N){
+        string S;
+        ll T;
+        cin >> S >> T;
+        mount.at(i) = {T, S};
+    }
+    sort(all(mount));
+    reverse(all(mount));
+    cout << mount.at(1).second << '\n';
+}
