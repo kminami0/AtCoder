0eb1070030f44105a1c3d91cc81348979fdfa39e
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-05-22 21:02:50 +0900

    [AC] abc202 abc202_b

---
+++ atcoder.jp/abc202/abc202_b/Main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+9
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    string S;
+    cin >> S;
+    reverse(all(S));
+    rep(i, sz(S)){
+        if(S.at(i) == '6'){
+            S.at(i) = '9';
+        }
+        else if(S.at(i) == '9'){
+            S.at(i) = '6';
+        }
+    }
+    cout << S << '\n';
+}
