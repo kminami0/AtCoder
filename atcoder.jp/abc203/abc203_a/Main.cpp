1a3bbf88ceb50df5e70694708cc84982b307b640
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-05-30 21:01:16 +0900

    [AC] abc203 abc203_a

---
+++ atcoder.jp/abc203/abc203_a/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+9
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    if(a == b){
+        cout << c << '\n';
+    }
+    else if(b == c){
+        cout << a << '\n';
+    }
+    else if(c == a){
+        cout << b << '\n';
+    }
+    else{
+        cout << 0 << '\n';
+    }
+}
