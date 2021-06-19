3f419ee12dbbb6fdeda5596e29146baf23d33d28
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-06-19 21:13:41 +0900

    [AC] abc206 abc206_d

---
+++ atcoder.jp/abc206/abc206_d/Main.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+vector<ll> par(1000000);
+vector<ll> rnk(1000000);
+
+void init(ll n){
+  rep(i, n){
+    par.at(i) = i;
+    rnk.at(i) = 0;
+  }
+}
+
+ll root(ll x){
+  if(par.at(x) == x){
+    return x;
+  }
+  else{
+    par.at(x) = root(par.at(x));
+    return par.at(x);
+  }
+}
+
+bool same(ll x, ll y){
+  if(root(x) == root(y)){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+void unite(ll x, ll y){
+  x = root(x);
+  y = root(y);
+  if(x == y){
+    return;
+  }
+  if(rnk.at(x) < rnk.at(y)){
+    par.at(x) = y;
+  }
+  else{
+    par.at(y) = x;
+    if(rnk.at(x) == rnk.at(y)){
+      rnk.at(x)++;
+    }
+  }
+}
+
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    rep(i, N) cin >> A.at(i);
+    init(200009);
+    ll ans = 0;
+    rep(i, N){
+        if(root(A.at(i)) != root(A.at(N-1-i))){
+            unite(A.at(i), A.at(N-1-i));
+            ans++;
+        }
+    }
+    cout << ans << '\n';
+}
