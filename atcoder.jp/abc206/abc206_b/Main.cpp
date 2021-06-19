5d080a6684b215e4ec2321c5f3ff55165b24baa9
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-06-19 21:03:54 +0900

    [AC] abc206 abc206_b

---
+++ atcoder.jp/abc206/abc206_b/Main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    ll i = 1;
+    ll money = 0;
+    while(true){
+        money += i;
+        if(money >= N){
+            cout << i << '\n';
+            return 0;
+        }
+        i++;
+    }
+}
