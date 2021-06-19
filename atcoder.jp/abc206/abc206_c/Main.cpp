9f14dc1c655d4283cc97239499de9d8f9cbabf96
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-06-19 21:07:20 +0900

    [AC] abc206 abc206_c

---
+++ atcoder.jp/abc206/abc206_c/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    rep(i, N) cin >> A.at(i);
+    map<ll, ll> cnt;
+    rep(i, N){
+        cnt[A.at(i)]++;
+    }
+    ll ans = 0;
+    for(auto x : cnt){
+        ans += (N - x.second) * x.second;
+    }
+    ans /= 2;
+    cout << ans << '\n';
+}
