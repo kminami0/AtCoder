723e960ddadc272ea293671b2c7dd683bbba9a6b
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-06-24 17:56:12 +0900

    [AC] typical90 typical90_bw

---
+++ atcoder.jp/typical90/typical90_bw/Main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+vector<pair<long long, long long> > prime_factorize(long long N) {
+  vector<pair<long long, long long> > res;
+  for (long long a = 2; a * a <= N; ++a) {
+    if (N % a != 0) continue;
+    long long ex = 0; // 指数
+    // 割れる限り割り続ける
+    while (N % a == 0) {
+     ++ex;
+      N /= a;
+    }
+    // その結果を push
+    res.push_back({a, ex});
+  }
+  // 最後に残った数について
+  if (N != 1) res.push_back({N, 1});
+  return res;
+}
+
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<pair<ll, ll>> pf = prime_factorize(N);
+    ll cnt = 0;
+    for(auto p : pf){
+        cnt += p.second;
+    }
+    ll ans = 0;
+    while(cnt > 1){
+        ans++;
+        cnt = (cnt+1)/2;
+    }
+    cout << ans << '\n';
+}
