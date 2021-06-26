f1e9f7d553e797108a0945d3dc4cdbbf774f96f1
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-06-26 21:01:26 +0900

    [AC] abc207 abc207_a

---
+++ atcoder.jp/abc207/abc207_a/Main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    vector<ll> num(3);
+    rep(i, 3) cin >> num.at(i);
+    sort(all(num));
+    cout << num.at(2)+num.at(1) << '\n';
+}
