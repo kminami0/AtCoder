4c881870959ac4241d015f0a36a5ded1c9bf7432
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-07-10 09:57:27 +0900

    [AC] abc132 abc132_a

---
+++ atcoder.jp/abc132/abc132_a/Main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    string S;
+    cin >> S;
+    map<char, ll> cnt;
+    rep(i, 4){
+        cnt[S.at(i)]++;
+    }
+    for(auto c : cnt){
+        if(c.second != 2){
+            cout << "No" << '\n';
+            return 0;
+        }
+    }
+    cout << "Yes" << '\n';
+}
