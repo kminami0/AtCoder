0913dcf68481cfe6ec4918b4632a7af3c63bed14
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-07-17 21:03:49 +0900

    [AC] abc210 abc210_b

---
+++ atcoder.jp/abc210/abc210_b/Main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    string S;
+    cin >> N >> S;
+    rep(i, N){
+        if(S.at(i) == '1'){
+            if(i % 2 == 0){
+                cout << "Takahashi" << '\n';
+            }
+            else{
+                cout << "Aoki" << '\n';
+            }
+            return 0;
+        }
+    }
+}
