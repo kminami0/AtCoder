f5e193b39273b6f1951456e0f160d3fe98ecad53
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-07-31 21:01:23 +0900

    [AC] abc212 abc212_a

---
+++ atcoder.jp/abc212/abc212_a/Main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll A, B;
+    cin >> A >> B;
+    if(B == 0){
+        cout << "Gold" << '\n';
+    }
+    else if(A == 0){
+        cout << "Silver" << '\n';
+    }
+    else{
+        cout << "Alloy" << '\n';
+    }
+}
