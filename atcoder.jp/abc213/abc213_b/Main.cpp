757cc3c8177d2c53e4b131777f074c6630df2577
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-08-08 21:03:25 +0900

    [AC] abc213 abc213_b

---
+++ atcoder.jp/abc213/abc213_b/Main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<pair<ll, ll>> score(N);
+    rep(i, N){
+        ll x;
+        cin >> x;
+        score.at(i) = {x, i+1};
+    }
+    sort(all(score));
+    cout << score.at(N-2).second << '\n';
+}
