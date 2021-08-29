3bdc810731138a71f12a2b5b501a965ad169b923
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-08-29 21:10:49 +0900

    [AC] abc216 abc216_c

---
+++ atcoder.jp/abc216/abc216_c/Main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    string ans = "";
+    while(N > 0){
+        if(N % 2 == 0){
+            ans += "B";
+            N /= 2;
+        }
+        else{
+            ans += "A";
+            N--;
+        }
+    }
+    reverse(all(ans));
+    cout << ans << '\n';
+}
