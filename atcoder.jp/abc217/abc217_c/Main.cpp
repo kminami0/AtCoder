bd1071eceef0e9a8d8927e86cd6ef724f4dc2f35
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-09-04 21:07:06 +0900

    [AC] abc217 abc217_c

---
+++ atcoder.jp/abc217/abc217_c/Main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> p(N);
+    rep(i, N) cin >> p.at(i);
+    vector<ll> Q(N);
+    rep(i, N){
+        Q.at(p.at(i)-1) = i+1;
+    }
+    rep(i, N-1){
+        cout << Q.at(i) << " ";
+    }
+    cout << Q.at(N-1) << '\n';
+}
