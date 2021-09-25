1a4ea17bd7285023a2e49af37794921bc06f21e5
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-09-25 09:56:33 +0900

    [AC] atc001 fft_c

---
+++ atcoder.jp/atc001/fft_c/Main.cpp
@@ -0,0 +1,57 @@
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define INF 1e18
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T& a, const T& b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <typename T>
+bool chmin(T& a, const T& b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> A(N + 1, 0), B(N + 1, 0);
+    rep(i, N) {
+        cin >> A.at(i + 1) >> B.at(i + 1);
+    }
+    vector<ll> c = convolution(A, B);
+    rep(i, 2 * N) {
+        cout << c.at(i + 1) << endl;
+    }
+}
