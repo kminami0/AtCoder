7b2278be49896065c71d5c15587fa4a72c715fa9
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2021-12-02 11:19:43 +0900

    [AC] abc229 abc229_b

---
+++ atcoder.jp/abc229/abc229_b/Main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll A, B;
+    cin >> A >> B;
+    while(A != 0 || B != 0){
+        if(A%10 + B%10 >= 10){
+            cout << "Hard" << '\n';
+            return 0;
+        }
+        A /= 10;
+        B /= 10;
+    }
+    cout << "Easy" << '\n';
+}
