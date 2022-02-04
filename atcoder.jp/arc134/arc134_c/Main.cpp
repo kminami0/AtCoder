2883503701377eeecd05eceeefffae52da18d539
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2022-02-04 15:02:53 +0900

    [AC] arc134 arc134_c

---
+++ atcoder.jp/arc134/arc134_c/Main.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) v.size()
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 998244353;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+  if ((x += a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator-=(const mint a) {
+  if ((x += mod-a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+  if (!t) return 1;
+  mint a = pow(t>>1);
+  a *= a;
+  if (t&1) a *= *this;
+  return a;
+  }
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+vector<mint> inv(201); 
+
+void calc(){
+    rep(i, 200){
+        mint x = 1;
+        mint y = i+1;
+        inv.at(i+1) = x / y;
+    }
+}
+
+mint mcom(ll n, ll k){ // nCk
+    if(n-k < 0){
+        return 0;
+    }
+    mint ans = 1;
+    rep(i, k){
+        ans *= n-i;
+    }
+    rep(i, k){
+        ans *= inv[k-i];
+    }
+    return ans;
+}
+
+int main()
+{
+    calc();
+    ll N, K;
+    cin >> N >> K;
+    vector<ll> a(N);
+    rep(i, N) cin >> a[i];
+    ll sum = 0;
+    rep(i, N-1){
+        sum += a[i+1];
+    }
+    mint prod = 1;
+    rep(i, N-1){
+        prod *= mcom(a[i+1]+K-1, K-1);
+    }
+    mint p = mcom(a[0]-sum-1, K-1);
+    cout << p * prod << '\n';
+}
