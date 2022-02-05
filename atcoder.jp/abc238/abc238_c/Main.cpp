f71db8aa8684d27d1b077d1a65a7b9785aa67aed
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2022-02-05 21:14:58 +0900

    [AC] abc238 abc238_c

---
+++ atcoder.jp/abc238/abc238_c/Main.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) ll(v.size())
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 998244353;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+  if ((x += a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator-=(const mint a) {
+  if ((x += mod-a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+  if (!t) return 1;
+  mint a = pow(t>>1);
+  a *= a;
+  if (t&1) a *= *this;
+  return a;
+  }
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+mint mpow(mint x, ll y){
+  if(y == 0){
+    return 1;
+  }
+  if(y % 2 == 0){
+    mint a = mpow(x, y / 2);
+    return a * a;
+  }
+  else{
+    mint a = mpow(x, y / 2);
+    return a * a * x;
+  }
+}
+
+int main()
+{
+    ll N;
+    cin >> N;
+    mint ans = 0;
+    string s = to_string(N);
+    rep(i, sz(s)-1){
+        ans += (mpow(10, i) * 9) * (mpow(10, i) * 9 + 1) / 2;
+    }
+    ans += (-mpow(10, sz(s)-1) + 1 + N) * (-mpow(10, sz(s)-1) + 2 + N) / 2;
+    cout << ans << '\n';
+}
