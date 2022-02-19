e4d46a989b115934ce474d7fdb7d2b2ad8ede2a1
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2022-02-19 21:01:49 +0900

    [AC] abc239 abc239_a

---
+++ atcoder.jp/abc239/abc239_a/Main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) ll(v.size())
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    long double H;
+    cin >> H;
+    cout << fixed << setprecision(20);
+    cout << sqrt(H * (12800000+H)) << '\n';
+}
