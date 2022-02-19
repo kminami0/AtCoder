a4b85684f32689efeb1ca0f5382cf4a8a7e4256c
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2022-02-19 21:18:04 +0900

    [AC] abc239 abc239_d

---
+++ atcoder.jp/abc239/abc239_d/Main.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) ll(v.size())
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+vector<pair<long long, long long> > prime_factorize(long long N) {
+  vector<pair<long long, long long> > res;
+  for (long long a = 2; a * a <= N; ++a) {
+    if (N % a != 0) continue;
+    long long ex = 0; // 指数
+    // 割れる限り割り続ける
+    while (N % a == 0) {
+     ++ex;
+      N /= a;
+    }
+    // その結果を push
+    res.push_back({a, ex});
+  }
+  // 最後に残った数について
+  if (N != 1) res.push_back({N, 1});
+  return res;
+}
+
+int main()
+{
+    ll A, B, C, D;
+    cin >> A >> B >> C >> D;
+    rep2(i, A, B+1){
+        rep2(j, C, D+1){
+            vector<pair<ll, ll>> pf = prime_factorize(i+j);
+            if(sz(pf) == 1){
+                if(pf.at(0).second == 1){
+                    break;
+                }
+            }
+            if(j == D){
+                cout << "Takahashi" << '\n';
+                return 0;
+            }
+        }
+    }
+    cout << "Aoki" << '\n';
+}
