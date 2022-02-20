06abba3c2c631dc499f518384dac7130c8e359f8
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2022-02-20 21:02:11 +0900

    [AC] abc240 abc240_b

---
+++ atcoder.jp/abc240/abc240_b/Main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) ll(v.size())
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    set<ll> a;
+    rep(i, N){
+        ll x;
+        cin >> x;
+        a.insert(x);
+    }
+    cout << sz(a) << '\n';
+}
