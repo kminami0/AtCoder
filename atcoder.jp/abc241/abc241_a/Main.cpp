56ae9921c50f109bc378b92b6fa57cc389ffd506
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2022-02-26 21:01:12 +0900

    [AC] abc241 abc241_a

---
+++ atcoder.jp/abc241/abc241_a/Main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) ll(v.size())
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    vector<ll> a(10);
+    rep(i, 10){
+        cin >> a.at(i);
+    }
+    ll ans = 0;
+    rep(i, 3){
+        ans = a.at(ans);
+    }
+    cout << ans << '\n';
+}
