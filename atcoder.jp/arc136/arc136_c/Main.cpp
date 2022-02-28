de669febba934b97b62f73ce40111b4861a37fe6
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2022-02-28 14:02:12 +0900

    [AC] arc136 arc136_c

---
+++ atcoder.jp/arc136/arc136_c/Main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) ll(v.size())
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    rep(i, N) cin >> A.at(i);
+    ll M = 0;
+    rep(i, N){
+        chmax(M, A.at(i));
+    }
+    ll D = 0;
+    rep(i, N-1){
+        D += abs(A.at(i+1)-A.at(i));
+    }
+    D += abs(A.at(0) - A.at(N-1));
+    D /= 2;
+    cout << max(M, D) << '\n';
+}
