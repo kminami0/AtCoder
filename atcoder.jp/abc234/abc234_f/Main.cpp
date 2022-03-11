943eeef757c445eec86b0a4c62eecb3c67d71173
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2022-03-11 11:38:43 +0900

    [AC] abc234 abc234_f

---
+++ atcoder.jp/abc234/abc234_f/Main.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) ll(v.size())
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+const int mod = 998244353;
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+  if ((x += a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator-=(const mint a) {
+  if ((x += mod-a.x) >= mod) x -= mod;
+  return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+  if (!t) return 1;
+  mint a = pow(t>>1);
+  a *= a;
+  if (t&1) a *= *this;
+  return a;
+  }
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+vector<mint> fac(3000003, 1);
+void calc(int n){
+  rep(i, n){
+    fac[i+1] = fac[i] * (i+1);
+  }
+  return;
+}
+
+// combination mod prime
+// https://www.youtube.com/watch?v=8uowVvQ_-Mo&feature=youtu.be&t=1619
+struct combination {
+  vector<mint> fact, ifact;
+  combination(int n):fact(n+1),ifact(n+1) {
+    assert(n < mod);
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
+  }
+  mint operator()(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n]*ifact[k]*ifact[n-k];
+  }
+} comb(5005);
+
+int main()
+{
+    calc(30000);
+    string S;
+    cin >> S;
+    vector<ll> cnt(26, 0);
+    rep(i, sz(S)){
+        cnt[S[i]-'a']++;
+    }
+    vector<vector<mint>> dp(27, vector<mint>(sz(S)+1, 0));
+    dp[0][0] = 1;
+    rep(i, 26){
+        rep(j, cnt[i]+1){
+            rep(k, sz(S)+1-j){
+                dp[i+1][j+k] += comb(j+k, j) * dp[i][k];
+            }
+        }
+    }
+    mint ans = 0;
+    rep(i, sz(S)){
+        ans += dp[26][i+1];
+    }
+    cout << ans << '\n';
+}
