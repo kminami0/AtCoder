a302eca4a99fda33af26e8fa0a1bbff9c9c807c1
agarai3 <minami.kiraku.83c@st.kyoto-u.ac.jp>
2022-03-12 21:04:38 +0900

    [AC] abc243 abc243_b

---
+++ atcoder.jp/abc243/abc243_b/Main.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s, n) for (ll i = (s); i < (ll)(n); i++)
+#define all(v) begin(v), end(v)
+#define sz(v) ll(v.size())
+#define INF 1e18+999999
+#define EPSILON 1e-14
+template <typename T>
+bool chmax(T &a, const T& b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T>
+bool chmin(T &a, const T& b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+int main()
+{
+    ll N;
+    cin >> N;
+    vector<ll> A(N);
+    rep(i, N) cin >> A.at(i);
+    vector<ll> B(N);
+    rep(i, N) cin >> B.at(i);
+    ll ans1 = 0;
+    ll ans2 = 0;
+    rep(i, N){
+        if(A.at(i) == B.at(i)){
+            ans1++;
+        }
+    }
+    rep(i, N){
+        rep(j, N){
+            if(i == j){
+                continue;
+            }
+            if(A.at(i) == B.at(j)){
+                ans2++;
+            }
+        }
+    }
+    cout << ans1 << '\n';
+    cout << ans2 << '\n';
+}
